12c6b9ee3fc3638afc065674901018c76e053205
akm831 <142600091+akm831@users.noreply.github.com>
2026-09-15 23:49:57 +0900

    Expose P2 percentile diagnostics through FFI

--- prototypes/flutter_native/native/include/prototype_bridge.h
+++ prototypes/flutter_native/native/include/prototype_bridge.h
@@ -1,35 +1,33 @@
 #pragma once
-
 #include <stdint.h>
-
 #if defined(_WIN32)
 #define PROTOTYPE_FFI_EXPORT __declspec(dllexport)
 #else
 #define PROTOTYPE_FFI_EXPORT __attribute__((visibility("default"))) __attribute__((used))
 #endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 typedef struct PrototypeDiagnostics {
     double sample_rate;
     uint32_t callback_frames;
+    uint32_t callback_frames_min;
+    uint32_t callback_frames_max;
     uint64_t rendered_frames;
     uint64_t callback_start_frame;
     double callback_duration_us;
     double callback_load;
+    double callback_load_p95;
+    double callback_load_p99;
     double callback_load_peak;
     uint32_t audio_restart_count;
 } PrototypeDiagnostics;
-
 PROTOTYPE_FFI_EXPORT void* prototype_create(void);
 PROTOTYPE_FFI_EXPORT void prototype_destroy(void* handle);
 PROTOTYPE_FFI_EXPORT void prototype_initialize(void* handle, double sample_rate, uint32_t max_callback_frames);
 PROTOTYPE_FFI_EXPORT int32_t prototype_start_audio(void* handle);
 PROTOTYPE_FFI_EXPORT void prototype_stop_audio(void* handle);
 PROTOTYPE_FFI_EXPORT PrototypeDiagnostics prototype_get_diagnostics(void* handle);
-
 #ifdef __cplusplus
 }
 #endif
